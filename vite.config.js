5c9a27a55d467d7e0ca8c708ca91b6e523edd354
Louis <louis@larger.io>
2024-11-20 12:37:00 +0100

    upgrade vite & inertiajs

--- vite.config.js
+++ vite.config.js
@@ -9,22 +9,35 @@ export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, "js/main.js"),
-            name: "Inertia.js Tables for Laravel Query Builder",
-            fileName: (format) => `inertiajs-tables-laravel-query-builder.${format}.js`
+            name: "InertiaJsTablesLaravelQueryBuilder",
+            fileName: (format) => `inertiajs-tables-laravel-query-builder.${format}.js`,
         },
         rollupOptions: {
             external: [
-                /^@inertiajs.*/,
-                /^@popperjs.*/,
-                /^lodash-es.*/,
-                "qs",
-                "vue"
+                /^@inertiajs\/.*/, // Externalize all @inertiajs modules
+                /^@popperjs\/.*/,  // Externalize all @popperjs modules
+                /^lodash-es\/.*/,  // Externalize all lodash-es modules
+                "qs",              // Externalize qs
+                "vue",             // Externalize Vue
             ],
             output: {
                 globals: {
-                    vue: "Vue"
-                }
-            }
-        }
-    }
-})
+                    vue: "Vue",
+                    "@popperjs/core/lib/popper-lite": "popperLite",
+                    "@popperjs/core/lib/modifiers/preventOverflow": "preventOverflow",
+                    "@popperjs/core/lib/modifiers/flip": "flip",
+                    "lodash-es/uniq": "uniq",
+                    "lodash-es/find": "find",
+                    "lodash-es/clone": "clone",
+                    "lodash-es/filter": "filter",
+                    "lodash-es/findKey": "findKey",
+                    "lodash-es/forEach": "forEach",
+                    "lodash-es/isEqual": "isEqual",
+                    "lodash-es/map": "map",
+                    "lodash-es/pickBy": "pickBy",
+                    qs: "qs",
+                },
+            },
+        },
+    },
+});
